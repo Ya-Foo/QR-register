40f49d46f16c4043338fa2425110213011ab716a
Ya-Foo <ghuynh2@bisvietnam.com>
2024-01-05 22:30:46 +0700

    added confirmation text

--- src/main.py
+++ src/main.py
@@ -10,15 +10,17 @@ class WebcamApp:
         self.window.title("QR Registration")
         
         # OpenCV setup
-        camera_id = 1
+        camera_id = 1  # 0 for front cam, 1 for back cam
         self.qcd = cv2.QRCodeDetector()
         self.video_capture = cv2.VideoCapture(camera_id)
         self.currentImage = None
         
         # Display
         root.state('zoomed')
-        self.canvas = tk.Canvas(window, width=screen_width, height=screen_height)
+        self.canvas = tk.Canvas(window, width=screen_width//2, height=screen_height//2)
         self.canvas.pack()
+        self.text = "Registered user: "
+        tk.Label(window, self.text).pack()
         
         self.update_Webcam()
         
@@ -26,16 +28,17 @@ class WebcamApp:
         returnValue, frame = self.video_capture.read()
         frame = cv2.resize(frame, (screen_width//2, screen_height//2))
         
-        # If using front came, uncomment
+        # If using front cam, uncomment below:
         # frame = cv2.flip(frame, 1)
         if returnValue:
             returnQR, decoded_info, points, _ = self.qcd.detectAndDecodeMulti(frame)
             
             if returnQR:
-                for s, p in zip(decoded_info, points):
-                    if s:
-                        if s not in scanned:
-                            scanned.append(s)
+                for data, p in zip(decoded_info, points):
+                    if data:
+                        self.text = f"Registered user: {data}"
+                        if data not in scanned:
+                            scanned.append(data)
                         
                         frame = cv2.polylines(frame, [p.astype(int)], True, (0, 255, 0), 8)
                     
@@ -43,7 +46,7 @@ class WebcamApp:
             self.photo = ImageTk.PhotoImage(image=self.currentImage)
             self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
             self.window.after(15, self.update_Webcam)
-        
+            
 
 # ============================================================= #
 # ========================== QR Scan ========================== #
